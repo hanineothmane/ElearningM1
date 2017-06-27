d0037eaec0592f3e9326a4699762cc8919d3b328
Hanine <hanineothmane@outlook.fr>
2017-06-27 09:55:39 +0200

    redefinition de la classe administration

--- ElearningM1/Models/Administration.cs
+++ ElearningM1/Models/Administration.cs
@@ -5,8 +5,26 @@ using System.Web;
 
 namespace ElearningM1.Models
 {
-    public class Administration
+    public class Administration : Profil
     {
+        public Administration(string nom, string dateNaiss, string prenom, string email, int id, string mdp, string telephone, string adresse) : base(nom, dateNaiss, prenom, email, id, mdp, telephone, adresse)
+        {
 
+        }
+
+        public void consulterInfoApprenant(Apprenant a)
+        {
+
+        }
+
+        public void consulterInfoTE(TuteurEnseignant te)
+        {
+
+        }
+
+        public void affecterApprenantModule(Apprenant a, Module m)
+        {
+
+        }
     }
 }
